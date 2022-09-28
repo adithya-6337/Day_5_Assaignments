cb42436f7361bb7b177a09f078b9c731b885497e
adithya-6337 <veldandiadithya337@gmail.com>
2022-09-28 11:40:25 +0530

    Work with Harmonic Number programming

---
+++ src/com/bridgelabz/HarmonicNumber.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class HarmonicNumber {
+    public static void main(String[] args) {
+        int num = 5;
+        double result = 0.0;
+        System.out.println("The harmonic series is: " + num);
+        while (num > 0) {
+            result = result + (double) 1 / num;
+            num--;
+            System.out.print(result + " , ");
+        }
+    }
+}
