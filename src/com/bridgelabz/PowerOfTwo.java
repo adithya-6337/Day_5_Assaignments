0617ffbd0b89cfb0ebbf932b54310524c0224ae4
adithya-6337 <veldandiadithya337@gmail.com>
2022-09-28 11:31:03 +0530

    Work with Power of 2 Programme

---
+++ src/com/bridgelabz/PowerOfTwo.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class PowerOfTwo {
+    public static void main(String[] args) {
+
+        // read in one command-line argument
+        int n = 5;
+        System.out.println(" 2 Power of :");
+        int i = 0;                // count from 0 to N
+        int powerOfTwo = 1;       // the ith power of two
+        // repeat until i equals n
+        while (i <= n) {
+            System.out.println(i + " " + powerOfTwo);
+            powerOfTwo = 2 * powerOfTwo;                // double to get the next one
+            i = i + 1;
+            // print out the power of two
+        }
+    }
+}
